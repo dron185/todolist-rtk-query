bd507bbeaceacb170564ebc949f5e7736f7b0b7f
Andrei Huryn <gurin.nord@gmail.com>
2024-11-19 20:40:47 +0300

    chore: replace authApi

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,22 +3,11 @@ import { authReducer, authSlice } from "../features/auth/model/authSlice"
 import { tasksReducer, tasksSlice } from "../features/todolists/model/tasksSlice"
 import { todolistsReducer, todolistsSlice } from "../features/todolists/model/todolistsSlice"
 import { appReducer, appSlice } from "./appSlice"
-import { _todolistsApi, todolistsApi } from "../features/todolists/api/todolistsApi"
 import { setupListeners } from "@reduxjs/toolkit/query"
-//
-// export const store = configureStore({
-//   reducer: {
-//     [tasksSlice.name]: tasksReducer,
-//     [todolistsSlice.name]: todolistsReducer,
-//     [appSlice.name]: appReducer,
-//     [authSlice.name]: authReducer,
-//   },
-// })
-//
+import { baseApi } from "./baseApi"
+
 export type RootState = ReturnType<typeof store.getState>
-
 export type AppDispatch = typeof store.dispatch
-
 
 export const store = configureStore({
   reducer: {
@@ -26,11 +15,9 @@ export const store = configureStore({
     [todolistsSlice.name]: todolistsReducer,
     [appSlice.name]: appReducer,
     [authSlice.name]: authReducer,
-    [todolistsApi.reducerPath]: todolistsApi.reducer,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
-  // 1
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(todolistsApi.middleware),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),
 })
 
-// 2
 setupListeners(store.dispatch)
--- src/features/auth/model/authSlice.ts
+++ src/features/auth/model/authSlice.ts
@@ -5,32 +5,32 @@ import { Dispatch } from "redux"
 import { setAppStatus } from "../../../app/appSlice"
 import { clearTasks } from "../../todolists/model/tasksSlice"
 import { clearTodolists } from "../../todolists/model/todolistsSlice"
-import { authApi } from "../api/authAPI"
+import { _authApi } from "../api/authAPI"
 import { LoginArgs } from "../api/authAPI.types"
 
 export const authSlice = createSlice({
   name: "auth",
   initialState: {
     isLoggedIn: false,
-    isInitialized: false,
+    // isInitialized: false,
   },
   reducers: (create) => ({
     setIsLoggedIn: create.reducer<{ isLoggedIn: boolean }>((state, action) => {
       state.isLoggedIn = action.payload.isLoggedIn
     }),
-    setIsInitialized: create.reducer<{ isInitialized: boolean }>((state, action) => {
-      state.isInitialized = action.payload.isInitialized
-    }),
+    // setIsInitialized: create.reducer<{ isInitialized: boolean }>((state, action) => {
+    //   state.isInitialized = action.payload.isInitialized
+    // }),
   }),
   selectors: {
     selectIsLoggedIn: (state) => state.isLoggedIn,
-    selectIsInitialized: (state) => state.isInitialized,
+    // selectIsInitialized: (state) => state.isInitialized,
   },
 })
 
 export const loginTC = (data: LoginArgs) => (dispatch: Dispatch) => {
   dispatch(setAppStatus({ status: "loading" }))
-  authApi
+  _authApi
     .login(data)
     .then((res) => {
       if (res.data.resultCode === ResultCode.Success) {
@@ -48,7 +48,7 @@ export const loginTC = (data: LoginArgs) => (dispatch: Dispatch) => {
 
 export const logoutTC = () => (dispatch: Dispatch) => {
   dispatch(setAppStatus({ status: "loading" }))
-  authApi
+  _authApi
     .logout()
     .then((res) => {
       if (res.data.resultCode === ResultCode.Success) {
@@ -68,7 +68,7 @@ export const logoutTC = () => (dispatch: Dispatch) => {
 
 export const initializeAppTC = () => (dispatch: Dispatch) => {
   dispatch(setAppStatus({ status: "loading" }))
-  authApi
+  _authApi
     .me()
     .then((res) => {
       if (res.data.resultCode === ResultCode.Success) {
@@ -82,10 +82,10 @@ export const initializeAppTC = () => (dispatch: Dispatch) => {
       handleServerNetworkError(error, dispatch)
     })
     .finally(() => {
-      dispatch(setIsInitialized({ isInitialized: true }))
+      // dispatch(setIsInitialized({ isInitialized: true }))
     })
 }
 
-export const { setIsLoggedIn, setIsInitialized } = authSlice.actions
-export const { selectIsLoggedIn, selectIsInitialized } = authSlice.selectors
+export const { setIsLoggedIn /*setIsInitialized*/ } = authSlice.actions
+export const { selectIsLoggedIn /*selectIsInitialized*/ } = authSlice.selectors
 export const authReducer = authSlice.reducer
