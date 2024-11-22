d9b1f323969c5f9b5c5399aff3b6f619ffd44d47
Andrei Huryn <gurin.nord@gmail.com>
2024-11-22 12:36:35 +0300

    chore: add extraReducers and addMatcher to appSlice.ts

--- src/features/todolists/ui/Todolists/Todolists.tsx
+++ src/features/todolists/ui/Todolists/Todolists.tsx
@@ -2,6 +2,7 @@ import Paper from "@mui/material/Paper"
 import Grid from "@mui/material/Unstable_Grid2"
 import { Todolist } from "./Todolist/Todolist"
 import { useGetTodolistsQuery } from "../../api/todolistsApi"
+import Skeleton from "@mui/material/Skeleton"
 
 export const Todolists = () => {
   // const todolists = useAppSelector(selectTodolists)
@@ -14,15 +15,17 @@ export const Todolists = () => {
 
   //const [skip, setSkip] = useState(true)
   //const { data: todolists } = useGetTodolistsQuery(undefined, { skip })
-
   // const [trigger, { data: todolists }] = useLazyGetTodolistsQuery()
-
   // const fetchTodolistHandler = () => {
   //   //setSkip(false)
   //   trigger()
   // }
 
-  const { data: todolists } = useGetTodolistsQuery()
+  const { data: todolists, isLoading } = useGetTodolistsQuery()
+
+  if (isLoading) {
+    return <Skeleton />
+  }
 
   return (
     <>
